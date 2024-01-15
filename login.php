fcf7095c6bcf8deb44e78fb8d6edb08f1de1a981
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2024-01-15 12:33:30 +0100

    added login

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Log In</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="well">
+                <center><h1 style="color: white">FoodBook</h1></center>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="main-content">
+                <div class="header">
+                    <h3><strong>Log in using your username and password!</strong></h3>
+                </div>
+                <div class="l-part">
+                    <form action="" method="post">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
+                            <input type="text" class="form-control" placeholder="Username" name="user_name">
+                        </div>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
+                            <input type="text" class="form-control" placeholder="Password" name="password">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- main.php
+++ main.php
@@ -40,13 +40,13 @@
                 <button id="signup" class="btn btn-info btn-lg" name="signup">Sign up</button></br></br>
                 <?php
                     if(isset($_POST['signup'])){
-                        echo "<script>window.open('signup.php','_self')</script>"
+                        echo "<script>window.open('signup.php','_self')</script>";
                     }
                 ?>
                 <button id="login" class="btn btn-info btn-lg" name="login">Log in</button></br></br>
                 <?php
                     if(isset($_POST['login'])){
-                        echo "<script>window.open('login.php','_self')</script>"
+                        echo "<script>window.open('login.php','_self')</script>";
                     }
                 ?>
             </form>
