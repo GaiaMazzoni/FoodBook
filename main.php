0832535736ac0391b00d17eb8a9e5006e5a2d8b3
matidan01 <matildedantino@gmail.com>
2024-01-16 13:38:49 +0100

    Changed dimension and color

--- main.php
+++ main.php
@@ -13,40 +13,44 @@
         overflow-x: hidden;
     }
     #signup{
-        width: 60%;
+        width: 200px;
         border-radius: 30px;
+        background-color: #fccf00;
+        border: 1px solid #4f0484;
+        color: #4f0484;
     }
     #login{
-        width: 60%;
+        width: 200px;
         border-radius: 30px;
-        background-color: #fff;
-        border: 1px solid #1da1f2;
-        color: #1da1f2;
+        background-color: #4f0484;
+        border: 1px solid #fccf00;
+        color: #fccf00;
+    }
+    form {
+        padding: 2%;
     }
 </style>
 <body>
     <div class="row">
-        <div class="col-sm-12">
-            <div class="well">
-                <center><h1>FoodBook</h1></center>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-       <div class="col-sm-6" style="left:8%;">
-            <img src="images/logo.png" alt="FoodBook" class="img-rounded" title="FoodBook logo" width="650px" 
-            height="565px">
+       <div class="col-sm-12 text-center">
+            <img src="images/logo.png" alt="FoodBook" class="img-rounded" title="FoodBook logo" width="200px" 
+            height="200px" id = "logo">
+            <h1>FoodBook</h1>
+            <p>Join us</p>
             <form method="post" action="">
                 <button id="signup" class="btn btn-info btn-lg" name="signup">Sign up</button></br></br>
                 <?php
-                    if(isset($_POST['signup'])){
-                        echo "<script>window.open('signup.php','_self')</script>"
+                    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['signup']))
+                    {
+                        header("Location: signup.php");
+                        exit;
                     }
                 ?>
                 <button id="login" class="btn btn-info btn-lg" name="login">Log in</button></br></br>
                 <?php
-                    if(isset($_POST['login'])){
-                        echo "<script>window.open('login.php','_self')</script>"
+                    if (isset($_POST['login'])) {
+                        header("Location: login.php");
+                        exit;
                     }
                 ?>
             </form>
