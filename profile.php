4ddbf9774c3b40c0d1b4e2ca27ab52216179f679
matidan01 <matildedantino@gmail.com>
2024-01-23 14:28:18 +0100

    From Bootstrap 3 to Bootstrap 5

--- profile.php
+++ profile.php
@@ -33,68 +33,22 @@ $num_follower = mysqli_num_rows($run_follower);
 $user_following = "select * from follow where Follower_Username='$user'";
 $run_following = mysqli_query($con,$user_following);
 $num_following = mysqli_num_rows($run_following);
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="functions.js"></script>
     <title><?php echo "$user Profile"; ?></title>
 </head>
-
-<script>
-
-document.addEventListener('DOMContentLoaded', function() {
-    popUpFunction('change_bio_btn', 'change_bio_form');
-    popUpFunction('edit_profile','edit_menu');
-    popUpFunction('change_img_btn','change_image_form');
-    popUpFunction('change_birthdate_btn','change_birthdate_form');
-    popUpFunction('change_email_btn','change_email_form');
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var logoutLink = document.getElementById('logout');
-    logoutLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        var confirmLogout = confirm("Are you sure you want to log out?");
-        if (confirmLogout) {
-            window.location.href = 'logout.php';
-        }
-    });
-});
-
-</script>
 <style>
 .position-relative {
     position: relative;
-}
-#edit_menu {
-    display: none;
-    position: absolute;
-    top: 100%; 
-    right: 75px;
-    background-color: white;
-    border: 1px solid #ddd;
-    z-index: 1000; 
-}
-
-#edit_menu > ul {
-    list-style-type: none;
-}
-#edit_menu > ul > li > a {
-    display: block;
-    padding: 10px;
-    text-decoration: none;
-    color: black;
-}
-
-#edit_menu > ul > li > a:hover {
-    background-color: #f8f8f8;
 }
 .container_form {
     position: relative;
@@ -115,40 +69,36 @@ document.addEventListener('DOMContentLoaded', function() {
     color: #000;
     text-decoration: none;
 }
+
 </style>
 <body>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col text-right position-relative">
-            <button id="edit_profile" class="btn btn-primary mt-3">Edit Profile</button>
-            <div id="edit_menu" style="display: none;">
-                <ul>
-                    <li><a href="#" id="change_img_btn">Change Image</a></li>
-                    <li><a href="#" id="change_bio_btn">Change Bio</a></li>
-                    <li><a href="#" id="change_email_btn">Change Email</a></li>
-                    <li><a href="#" id="change_birthdate_btn">Change Birthdate</a></li>
-                    <li><a href="#" id="change_name">Change Name</a></li>
-                </ul>
-            </div>
-            <button id="logout" class="btn btn-primary mt-3">Logout</button>
-        </div>
-    </div>
+<div class="dropdown">
+    <button type="button" id="edit_profile" class="btn btn-primary btn-sm float-end mt-3dropdown-toggle" data-bs-toggle="dropdown"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+</svg> Edit Profile</button>
+    <ul class="dropdown-menu" id="edit_menu">
+        <li><button type="button" class="btn btn-primary dropdown-item" data-bs-toggle="modal" data-bs-target="#change_image_form" id="change_image_btn">Change Image</button></li>
+        <li><button type="button" class="btn btn-primary dropdown-item" data-bs-toggle="modal" data-bs-target="#change_bio_form" id="change_bio_btn">Change Bio</button></li>
+        <li><button type="button" class="btn btn-primary dropdown-item" data-bs-toggle="modal" data-bs-target="#change_email_form" id="change_email_btn">Change Email</button></li>
+        <li><button type="button" class="btn btn-primary dropdown-item" data-bs-toggle="modal" data-bs-target="#change_birthdate_form" id="change_birthdate_btn">Change BirthDate</button></li>
+        <li><button type="button" class="btn btn-primary dropdown-item" data-bs-toggle="modal" data-bs-target="#change_name_form" id="change_name_btn">Change Name</button></li>
+    </ul>
 </div>
-<div class="container_form">
-<?php
-    generateForm('bio', 'Bio', 'bio', 150);
-    generateForm('email', 'Email', 'email', 30);
-    generateForm('birthdate', 'BirthDate', 'birthdate');
-    generateForm('image', 'Image', 'image');
-    generateForm('name', 'First-Name', 'firstname', 20);
+
+
+<?php 
+echo generateModalForm('bio', 'Bio', 150);
+echo generateModalForm('email', 'Email', 30);
+echo generateModalForm('birthdate', 'BirthDate');
+echo generateModalForm('image', 'Image'); 
 ?>
-</div>
+
 <div class="container">
     <div class="row">
-        <div class="col-xs-6 col-md-3">
+        <div class="col-12 col-md-3 text-center">
             <img src="images/<?php echo $profile_picture; ?>" class="img-fluid rounded-circle mb-3" id="p_profile" alt="Immagine Profilo" style="width: 200px; height: 200px; object-fit: cover;">
         </div>
-        <div class="col-xs-6 col-md-3">
+        <div class="col-12 col-md-3 text-center">
             <h3 class="card-title"><?php echo $first_name . " " . $surname; ?></h3>
             <p class="text-muted">@<?php echo $user; ?></p>
             <p class="card-text">
@@ -156,15 +106,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 <strong>Data di nascita:</strong> <?php echo $birth_date; ?><br>
             </p>
             <div class="row">
-                <div class="col-xs-4 text-center">
+                <div class="col-4 text-center">
                     <?php echo $num_posts; ?>
                     <p>post</p>
                 </div>
-                <div class="col-xs-4 text-center">
+                <div class="col-4 text-center">
                     <?php echo $num_follower; ?>
                     <p>follower</p>
                 </div>
-                <div class="col-xs-4 text-center">
+                <div class="col-4 text-center">
                     <?php echo $num_following; ?>
                     <p>following</p>
                 </div>
