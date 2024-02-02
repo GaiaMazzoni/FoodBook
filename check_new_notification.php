2efd0fcb5c933954a6865b0762ae824e37fefcce
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2024-02-02 20:58:10 +0100

    started implementing logics to change notification icon

--- check_new_notification.php
+++ check_new_notification.php
@@ -2,12 +2,10 @@
 include 'functions.php';
 include 'includes/connection.php';
 
-echo "reading";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $allNotif = get_all_unread_notifications($_POST['usernameTo'], $con);
-    foreach ($allNotif as $n) {
-        echo $n['DateAndTime'];
-        read_notification($n, $con);
+    if($allNotif != NULL){
+        echo "true";
     }
-    
 }
--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,30 @@
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
 <script>
+let notificationsButton;
+let notificationicon;
+let notificationPath;
+let usernameTo;
+document.addEventListener('DOMContentLoaded', function(){
+    notificationsButton = document.getElementById('notification-button');
+    notificationicon = document.getElementById('notificationIcon');
+    notificationPath = document.getElementById('notificationPath');
+    usernameTo = '<?php echo $_SESSION['Username']?>';
+
+    let checkNewNotifFormData = new FormData();
+    checkNewNotifFormData.append('usernameTo', usernameTo);
+    axios.post("check_new_notification.php", checkNewNotifFormData).then(response => {
+        console.log(response.data);
+        if(response.data == true){
+            if(!notificationsButton.classList.contains('newNotification')){
+                notificationsButton.classList.add('newNotification');
+                notificationicon.setAttribute('fill', 'red');
+                notificationPath.setAttribute('d', "M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901");
+            }
+        }
+    });
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     var logoutLink = document.getElementById('logout');
     logoutLink.addEventListener('click', function(event) {
@@ -25,25 +49,19 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    var notificationsButton = document.getElementById('notification-button');
-    var notificationicon = document.getElementById('notificationIcon');
-    var notificationPath = document.getElementById('notificationPath');
-    
+    notificationsButton = document.getElementById('notification-button');
+    notificationicon = document.getElementById('notificationIcon');
+    notificationPath = document.getElementById('notificationPath');
+    usernameTo = '<?php echo $_SESSION['Username']?>';
     notificationsButton.addEventListener('click', function(event) {
-        /*if (notificationsButton.classList.contains('newNotification')) {
+        if (notificationsButton.classList.contains('newNotification')) {
             notificationsButton.classList.remove('newNotification');
-            notificationIcon.setAttribute('fill', 'currentColor');
+            notificationicon.setAttribute('fill', 'currentColor');
             notificationPath.setAttribute('d', "M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6");
-        } else {
-            notificationsButton.classList.add('newNotification');
-            notificationIcon.setAttribute('fill', 'red');
-            notificationPath.setAttribute('d', "M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901");
-        }*/
-        let usernameTo = '<?php echo $_SESSION['Username']?>';
+        }
         let formData = new FormData();
         formData.append('usernameTo', usernameTo);
-        axios.post("check_notifications.php", formData).then(response => {
-            console.log(response.data);
+        axios.post("read_notifications.php", formData).then(response => {
         });
         
     });
