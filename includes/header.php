25c6ccf784c1df04900ea2b0728a968974ced34e
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2024-02-02 22:27:15 +0100

    adjusted some details

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     include_once "functions.php";
+    include_once "includes/header.php";
 ?>
 
 <!DOCTYPE html>
@@ -35,9 +36,6 @@ document.addEventListener('DOMContentLoaded', function(){
             }
         }
     });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
     var logoutLink = document.getElementById('logout');
     logoutLink.addEventListener('click', function(event) {
         event.preventDefault();
@@ -46,6 +44,24 @@ document.addEventListener('DOMContentLoaded', function() {
             window.location.href = 'logout.php';
         }
     });
+
+    notificationsButton.addEventListener('click', function(event) {
+        
+        let formData = new FormData();
+        formData.append('usernameTo', usernameTo);
+        axios.post("read_notifications.php", formData).then(response => {
+        });
+        if (notificationsButton.classList.contains('newNotification')) {
+            notificationsButton.classList.remove('newNotification');
+            notificationicon.setAttribute('fill', 'currentColor');
+            notificationPath.setAttribute('d', "M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6");
+        }
+        
+    });
+});
+
+/*document.addEventListener('DOMContentLoaded', function() {
+    
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -53,19 +69,8 @@ document.addEventListener('DOMContentLoaded', function() {
     notificationicon = document.getElementById('notificationIcon');
     notificationPath = document.getElementById('notificationPath');
     usernameTo = '<?php echo $_SESSION['Username']?>';
-    notificationsButton.addEventListener('click', function(event) {
-        if (notificationsButton.classList.contains('newNotification')) {
-            notificationsButton.classList.remove('newNotification');
-            notificationicon.setAttribute('fill', 'currentColor');
-            notificationPath.setAttribute('d', "M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6");
-        }
-        let formData = new FormData();
-        formData.append('usernameTo', usernameTo);
-        axios.post("read_notifications.php", formData).then(response => {
-        });
-        
-    });
-});
+    
+});*/
 
 </script>
 <style>
