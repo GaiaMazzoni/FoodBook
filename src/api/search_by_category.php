d8200e52572e86b461c8e0a1bb8d09a731f85300
matidan01 <matildedantino@gmail.com>
2024-02-05 18:05:12 +0100

    Tried to fix search error

--- src/api/search_by_category.php
+++ src/api/search_by_category.php
@@ -4,6 +4,7 @@ include_once("../includes/functions.php");
 include_once("../includes/database.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    error_log("ciao". PHP_EOL, 3, 'log.txt');
     $num_cat = $_POST['num_cat'];
     $cat_list = [];
     for($i = 1; $i < $num_cat+1; $i++) {
@@ -11,17 +12,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $cat_list[] = get_id_category($cat_name, $con);
     }
     $result = get_posts_from_category($cat_list, $num_cat, $con);
-    $print = "<?php";
+    
+    $res = "";
 
     foreach($result as $post) {
-        $print .= print_base_post($post['Username'],$post['IdPost'],$con);
-        //$print .= ";";
+        $res .= print_base_post($post['Username'],$post['IdPost'],$con);
+        error_log("stampo_post", 3, './log.txt');
     }
 
-    $print .= "?>";
-
-    error_log($print . PHP_EOL, 3, "log.txt");
-
+    $res .= "";
+    
+    error_log("mando", 3, './log.txt');
     header('Content-Type: application/json');
-    echo json_encode("$print");
+    echo json_encode($res);
 } 
