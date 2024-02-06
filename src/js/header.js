18e4a4781dcb716d05ae9739a849380c97f35c04
matidan01 <matildedantino@gmail.com>
2024-02-06 23:16:23 +0100

    Added notification_logics.php

--- src/js/header.js
+++ src/js/header.js
@@ -8,10 +8,10 @@ document.addEventListener('DOMContentLoaded', function(){
     notificationPath = document.getElementById('notificationPath');
 
     let checkNewNotifFormData = new FormData();
-    axios.post("../api/check_new_notification.php", checkNewNotifFormData).then(response => {
+    checkNewNotifFormData.append("type", "check");
+    axios.post("../api/notification_logics.php", checkNewNotifFormData).then(response => {
         if(response.data == "true"){
             if(!notificationsButton.classList.contains('newNotification')){
-                
                 notificationsButton.classList.add('newNotification');
                 notificationicon.setAttribute('fill', 'red');
                 notificationPath.setAttribute('d', "M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901");
@@ -35,7 +35,8 @@ document.addEventListener('DOMContentLoaded', function(){
             notificationPath.setAttribute('d', "M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6");
         }
         let formData = new FormData();
-        axios.post("../api/read_notifications.php", formData).then(response => {
+        formData.append("type", "read");
+        axios.post("../api/notification_logics.php", formData).then(response => {
         });
         
     });
